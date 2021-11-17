dbfb0384fef969444e51c871ca573c4e1ff6a33d
Maria <maria.arsenkova@gmail.com>
2021-11-17 20:42:32 +0300

    Add automatic height textarea

--- src/Components/ChatFooter/index.tsx
+++ src/Components/ChatFooter/index.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import './style.scss'
 
 const ChatFooter = () => {
+    const handleTextAreaHeight = (event:any) => {
+        event.target.style.height = "auto";
+        event.target.style.height = event.target.scrollHeight + "px";
+    }
     return(
     <textarea
         autoFocus={true}
         rows={1}
         className='ChatFooter__enter-text-message'
         placeholder="Enter text message..."
+        onChange={handleTextAreaHeight}
     />
 
 )
